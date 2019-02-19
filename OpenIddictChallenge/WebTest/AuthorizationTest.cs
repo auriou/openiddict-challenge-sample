5f9156187812bcef1533c308997d7c5f492dfd73
Philippe AURIOU <p.auriou@live.fr>
2019-02-19 14:26:09 +0100

    save

--- OpenIddictChallenge/WebTest/AuthorizationTest.cs
+++ OpenIddictChallenge/WebTest/AuthorizationTest.cs
@@ -9,22 +9,34 @@ namespace SRPIntranet.IntegrationTest
     {
 
         [TestMethod]
+        public void Connect_Challenge()
+        {
+            var service = new SrbWebOpenIdConnect("http://localhost:44300", "openid offline_access");
+            service.Connect("pau", "pwd", "/connect/token");
+
+            var resWithLogin = service.TestApi("/api/message", HttpMethod.Get);
+            resWithLogin.Should().NotBeNullOrEmpty();
+
+            // challenge on WebApiClient
+            var resOtherApi = service.TestApi("http://localhost:44301/api/message", HttpMethod.Get);
+            resOtherApi.Should().NotBeNullOrEmpty();
+        }
+
+
+        [TestMethod]
         public void Connect_Refresh()
         {
             var service = new SrbWebOpenIdConnect("http://localhost:44300", "openid offline_access");
             service.Connect("pau", "pwd", "/connect/token");
             service.TokenOpenId.RefreshToken.Should().NotBeNullOrEmpty();
-            
-            var roles = service.GetClaim("role");
+
+            var roles = service.GetClaim("name");
 
             var resWithLogin = service.TestApi("/api/message", HttpMethod.Get);
             resWithLogin.Should().NotBeNullOrEmpty();
 
-            //service.Refresh("/connect/token", service.TokenOpenId.RefreshToken);
-            //var resWithRefresh = service.TestUrl("/api/ClientMenu", HttpMethod.Get);
-
-            // challenge on WebApiClient
-            var test = service.TestApi("http://localhost:44301/api/message", HttpMethod.Get);
+            service.Refresh("/connect/token", service.TokenOpenId.RefreshToken);
+            var resWithRefresh = service.TestApi("/api/message", HttpMethod.Get);
         }
     }
 }
